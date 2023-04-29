045395be3a2f09396c296abe8861cef45e02e038
Michal Nowakowski <nowakowski.m1995@gmail.com>
2023-04-29 18:41:02 +0200

    tests refactored

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,41 +1,54 @@
-import { test, expect } from '@playwright/test';
-
+import { test, expect } from "@playwright/test";
 
 test.describe("User login funcionality", () => {
+  // Arrange - global
+  const url = "https://demo-bank.vercel.app/";
+  const userPassword = "testtest";
+  const userLogin = "tester12";
 
-  test('001 Valid login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await expect(page.getByTestId('login-input')).toBeVisible();
-    await expect(page.getByTestId('password-input')).toBeVisible();
-    await page.getByTestId('login-input').fill('adminadm');
-    await page.getByTestId('password-input').fill('haslohas');
-    await page.getByTestId('login-button').click();
-    await expect(page.getByTestId('user-name')).toBeVisible();
-    await expect(page.getByTestId('user-name')).toHaveText("Jan Demobankowy")
+  test("001 Valid login with correct credentials", async ({ page }) => {
+    // Arrange
+    const expectedUsername = "Jan Demobankowy";
 
-  })
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userLogin);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
 
-  test('002 Invalid login with too short username', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await expect(page.getByTestId('login-input')).toBeVisible();
-    await expect(page.getByTestId('password-input')).toBeVisible();
-    await page.getByTestId('login-input').fill('admin');
-    await page.getByTestId('password-input').fill('haslohas');
-    await expect(page.getByTestId('error-login-id')).toBeVisible();
-    await expect(page.getByTestId('error-login-id')).toHaveText("identyfikator ma min. 8 znaków");
-    await expect(page.getByTestId('login-button')).toBeDisabled();
+    // Assert
+    await expect(page.getByTestId("user-name")).toBeVisible();
+    await expect(page.getByTestId("user-name")).toHaveText(expectedUsername);
+  });
+
+  test("002 Invalid login with too short username", async ({ page }) => {
+    // Arrange
+    const invalidUserLogin = "admin";
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(invalidUserLogin);
+    await page.getByTestId("password-input").fill(userPassword);
+    // Assert
+    await expect(page.getByTestId("error-login-id")).toBeVisible();
+    await expect(page.getByTestId("error-login-id")).toHaveText(
+      "identyfikator ma min. 8 znaków"
+    );
+    await expect(page.getByTestId("login-button")).toBeDisabled();
+  });
+
+  test("003 Invalid login with too short password", async ({ page }) => {
+    // Arrange
+    const invalidUserPassword = "haslo";
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userLogin);
+    await page.getByTestId("password-input").fill(invalidUserPassword);
+    await page.getByTestId("password-input").blur(); // Usuwa focus z elementu
+    // Assert
+    await expect(page.getByTestId("error-login-password")).toBeVisible();
+    await expect(page.getByTestId("error-login-password")).toHaveText(
+      "hasło ma min. 8 znaków"
+    );
+    await expect(page.getByTestId("login-button")).toBeDisabled();
+  });
 });
-
-  test('003 Invalid login with too short password', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await expect(page.getByTestId('login-input')).toBeVisible();
-    await expect(page.getByTestId('password-input')).toBeVisible();
-    await page.getByTestId('login-input').fill('adminadm');
-    await page.getByTestId('password-input').fill('haslo');
-    await page.getByTestId('password-input').blur() // Usuwa focus z elementu
-    await expect(page.getByTestId('error-login-password')).toBeVisible();
-    await expect(page.getByTestId('error-login-password')).toHaveText("hasło ma min. 8 znaków");
-    await expect(page.getByTestId('login-button')).toBeDisabled();
-});
-
-})
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,36 +1,57 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.describe("pulpit test", () => {
-    test('001 Valid quick transfer with correct data', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app');
+  // Arrange - global
+  const url = "https://demo-bank.vercel.app/";
+  const userLogin = "tester12";
+  const userPassword = "testtest";
 
-        await page.getByTestId('login-input').fill('adminadm');
-        await page.getByTestId('password-input').fill('haslohas');
-        await page.getByTestId('login-button').click();
+  test("001 Valid quick transfer with correct data", async ({ page }) => {
+    // Arrange
+    const recieverId = "2";
+    const transferAmount = "150";
+    const transferTitle = "Zwrot środków";
+    const expectedTransferReciever = "Chuck Demobankowy";
+    // Act
+    await page.goto(url);
+    await page.getByTestId("login-input").fill(userLogin);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
 
-        await page.locator('#widget_1_transfer_receiver').selectOption('2');
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
+    await page.locator("#widget_1_transfer_receiver").selectOption(recieverId);
+    await page.locator("#widget_1_transfer_amount").fill(transferAmount);
+    await page.locator("#widget_1_transfer_title").fill(transferTitle);
 
-        await page.getByRole('button', { name: 'wykonaj' }).click();
-        await page.getByTestId('close-button').click();
-        
+    await page.getByRole("button", { name: "wykonaj" }).click();
+    await page.getByTestId("close-button").click();
 
-        await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - Zwrot środków')
-      });
+    // Assert
+    await expect(page.locator("#show_messages")).toHaveText(
+      `Przelew wykonany! ${expectedTransferReciever} - ${transferAmount},00PLN - ${transferTitle}`
+    );
+  });
 
-      test('002 Valid mobile top-up with correct data', async ({ page }) => {
-        await page.goto('https://demo-bank.vercel.app/');
+  test("002 Valid mobile top-up with correct data", async ({ page }) => {
+    // Arrange
+    const topupRevieverNumber = "502 xxx xxx";
+    const topupAmount = "30";
+    // Act
+    await page.goto(url);
 
-        await page.getByTestId('login-input').fill('12112111');
-        await page.getByTestId('password-input').fill('11121212');
-        await page.getByTestId('login-button').click();
-        await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
-        await page.locator('#widget_1_topup_amount').fill('30');
-        await page.locator('#uniform-widget_1_topup_agreement span').click();
-        await page.getByRole('button', { name: 'doładuj telefon' }).click();
-        await page.getByTestId('close-button').click();
+    await page.getByTestId("login-input").fill(userLogin);
+    await page.getByTestId("password-input").fill(userPassword);
+    await page.getByTestId("login-button").click();
 
-        await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 30,00PLN na numer 502 xxx xxx')
-      });
-})
+    await page
+      .locator("#widget_1_topup_receiver")
+      .selectOption(topupRevieverNumber);
+    await page.locator("#widget_1_topup_amount").fill(topupAmount);
+    await page.locator("#uniform-widget_1_topup_agreement span").click();
+    await page.getByRole("button", { name: "doładuj telefon" }).click();
+    await page.getByTestId("close-button").click();
+    // Assert
+    await expect(page.getByTestId("message-text")).toHaveText(
+      `Doładowanie wykonane! ${topupAmount},00PLN na numer ${topupRevieverNumber}`
+    );
+  });
+});
