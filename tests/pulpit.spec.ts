504544f603c23e88201a6591d532d07dbd1b1247
Michal Nowakowski <nowakowski.m1995@gmail.com>
2023-04-29 18:03:09 +0200

    new test

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -18,4 +18,19 @@ test.describe("pulpit test", () => {
 
         await expect(page.locator("#show_messages")).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - Zwrot środków')
       });
+
+      test('002 Valid mobile top-up with correct data', async ({ page }) => {
+        await page.goto('https://demo-bank.vercel.app/');
+
+        await page.getByTestId('login-input').fill('12112111');
+        await page.getByTestId('password-input').fill('11121212');
+        await page.getByTestId('login-button').click();
+        await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
+        await page.locator('#widget_1_topup_amount').fill('30');
+        await page.locator('#uniform-widget_1_topup_agreement span').click();
+        await page.getByRole('button', { name: 'doładuj telefon' }).click();
+        await page.getByTestId('close-button').click();
+
+        await expect(page.getByTestId('message-text')).toHaveText('Doładowanie wykonane! 30,00PLN na numer 502 xxx xxx')
+      });
 })
